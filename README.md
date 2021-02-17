79ca3cddde4ae80d2250f1723c338c8fb447ac26
Vincent WIjaya <weqqss@yahoo.com>
2021-02-18 00:05:47 +0700

    update web

--- README.md
+++
@@ -1,7 +0,0 @@
-# vincentwijaya.github.io
-
-About This Site:
-My potofolio site hosted by Git
-
-About Me:
-A guy that love travelling and also playing games :)
--- styles-cdf1758d0ab247a91d61.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{179:function(n,o,w){},181:function(n,o,w){}}]);
-//# sourceMappingURL=styles-cdf1758d0ab247a91d61.js.map
